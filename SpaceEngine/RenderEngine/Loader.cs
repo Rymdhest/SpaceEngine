601333532303c0e0bfb3992d1ade659afc767e7c
Gustav <gustav.holmstroem@gmail.com>
2023-05-30 14:53:41 +0200

    Add project files.

---
+++ SpaceEngine/RenderEngine/Loader.cs
@@ -0,0 +1,50 @@
+﻿
+
+using OpenTK.Graphics.OpenGL4;
+
+namespace SpaceEngine.RenderEngine
+{
+    internal class Loader
+    {
+        public static Model loadToVAO(float[] positions, float[] colors, int[] indices)
+        {
+            int vaoID = createVAO();
+            int[] VBOS = new int[3];
+            VBOS[2] = bindIndicesBuffer(indices);
+
+            VBOS[0] = storeDataInAttributeList(0, 3, positions);
+            VBOS[1] = storeDataInAttributeList(1, 3, colors);
+            unbindVAO();
+            return new Model(vaoID, VBOS, indices.Length);
+        }
+
+        private static int createVAO()
+        {
+            int vaoID = GL.GenVertexArray();
+            GL.BindVertexArray(vaoID);
+            return vaoID;
+        }
+        private static int bindIndicesBuffer(int[] indices)
+        {
+            int vboID = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, vboID);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticCopy);
+            return vboID;
+        }
+        private static int storeDataInAttributeList(int attributeNumber, int coordinateSize, float[] data)
+        {
+            int vboID = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer,vboID);
+            GL.BufferData(BufferTarget.ArrayBuffer, data.Length * sizeof(float), data, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(attributeNumber, coordinateSize, VertexAttribPointerType.Float, false, 0, 0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            return vboID;
+        }
+        private static void unbindVAO()
+        {
+            GL.BindVertexArray(0);
+        }
+    }
+
+}
---
+++ SpaceEngine/RenderEngine/Model.cs
@@ -0,0 +1,39 @@
+﻿
+
+using OpenTK.Graphics.OpenGL4;
+
+namespace SpaceEngine.RenderEngine
+{
+    internal class Model
+    {
+        private int vaoID;
+        private int[] VBOS;
+        private int vertexCount;
+
+        public Model(int vaoID, int[] VBOS, int vertexCount)
+        {
+            this.vaoID = vaoID;
+            this.VBOS = VBOS;
+            this.vertexCount = vertexCount;
+        }
+
+        public int getVAOID()
+        {
+            return vaoID;
+        }
+
+        public int getVertexCount()
+        {
+            return vertexCount;
+        }
+
+        public void cleanUp()
+        {
+            GL.DeleteVertexArray(vaoID);
+            for (int i = 0; i<VBOS.Length; i++)
+            {
+                GL.DeleteBuffer(VBOS[i]);
+            }
+        }
+    }
+}
